d4547d08722b022c4b4075885771dc4d698771c1
tyra <tyragipson50@gmail.com>
2023-03-08 11:18:41 -0600

    added camera code

--- src/main/java/frc/robot/teleop/Robot.java
+++ src/main/java/frc/robot/teleop/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
 
 public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;  
@@ -20,6 +21,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    CameraServer.startAutomaticCapture();
   }
   
 @Override
